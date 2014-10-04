c905b4deb1f76a39374540e3d9c19b6fb0ec1c26
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2014-10-04 11:22:34 +0100

    Initial create of setup script
    Possibly wrong location

---
+++ script.sh
@@ -0,0 +1,7 @@
+# Simple script to grab a few of the extra bits of software for the Pi2Go
+
+wget -q http://4tronix.co.uk/pi2go/servod.xxx -O servod
+wget -q http://4tronix.co.uk/pi2go/Adafruit_PWM_Servo_Driver.py -O Adafruit_PWM_Servo_Driver.py
+wget -q http://4tronix.co.uk/pi2go/Adafruit_I2C.py -O Adafruit_I2C.py
+wget -q http://4tronix.co.uk/pi2go/sgh_PCF8591P.pyc -O sgh_PCF8591P.pyc
+chmod +x servod
