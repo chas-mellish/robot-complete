be364aa1a0c8fc117ac12a2ed3c0f1f6a1e6038a
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2014-10-08 21:31:14 +0100

    Update sh script

--- code/script.sh
+++ code/script.sh
@@ -1,9 +1,12 @@
 # Simple script to install a few bits of software for the Pi2Go-lite
 # I have left the Pi2Go bits in - just so that everything works in the library
 # Also some people will want to play around with the extra stuff!
+# Essentially you will download this twice but I thought that it was best for clarity and neatness
+# Plus I have nowhere else *reliable* to host it!
 
+echo "Just installing that stuff for you sir/madam! I'll be finished in a second!"
 wget -q https://github.com/the-raspberry-pi-guy/pi2go/blob/master/extra_software/servod.xxx -O servod
-wget -q http://4tronix.co.uk/pi2go/Adafruit_PWM_Servo_Driver.py -O Adafruit_PWM_Servo_Driver.py
-wget -q http://4tronix.co.uk/pi2go/Adafruit_I2C.py -O Adafruit_I2C.py
-wget -q http://4tronix.co.uk/pi2go/sgh_PCF8591P.pyc -O sgh_PCF8591P.pyc
+wget -q https://github.com/the-raspberry-pi-guy/pi2go/blob/master/extra_software/Adafruit_PWM_Servo_Driver.py -O Adafruit_PWM_Servo_Driver.py
+wget -q https://github.com/the-raspberry-pi-guy/pi2go/blob/master/extra_software/Adafruit_I2C.py -O Adafruit_I2C.py
+wget -q https://github.com/the-raspberry-pi-guy/pi2go/blob/master/extra_software/sgh_PCF8591P.pyc -O sgh_PCF8591P.pyc
 chmod +x servod
