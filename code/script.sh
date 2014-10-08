de3e8b477cc3c8f178848f2b297f68d082e3c1af
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2014-10-08 20:14:43 +0100

    Decided against keeping servod in the main code directory

--- code/script.sh
+++ code/script.sh
@@ -2,7 +2,7 @@
 # I have left the Pi2Go bits in - just so that everything works in the library
 # Also some people will want to play around with the extra stuff!
 
--O servod
+wget -q https://github.com/the-raspberry-pi-guy/pi2go/blob/master/extra_software/servod.xxx -O servod
 wget -q http://4tronix.co.uk/pi2go/Adafruit_PWM_Servo_Driver.py -O Adafruit_PWM_Servo_Driver.py
 wget -q http://4tronix.co.uk/pi2go/Adafruit_I2C.py -O Adafruit_I2C.py
 wget -q http://4tronix.co.uk/pi2go/sgh_PCF8591P.pyc -O sgh_PCF8591P.pyc
