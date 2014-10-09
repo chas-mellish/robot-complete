8752904029b3e5c1f79a6ee44887b68c70b5177c
muddyfish <simon.d.beal@gmail.com>
2014-10-09 13:23:32 +0000

    Created as a package

--- code/pi2go.py
+++ code/pi2go.py
@@ -90,8 +90,8 @@
 #======================================================================
 # Import all necessary libraries
 import RPi.GPIO as GPIO, sys, threading, time, os
-from Adafruit_PWM_Servo_Driver import PWM
-from sgh_PCF8591P import sgh_PCF8591P
+from extra_software.Adafruit_PWM_Servo_Driver import PWM
+from extra_software.sgh_PCF8591P import sgh_PCF8591P
 
 # Define Type of Pi2Go
 PGNone = 0
