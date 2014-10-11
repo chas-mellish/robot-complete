9ebad4e6ab5af7f0d890a59cb59c726489cf8b62
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2014-10-11 21:52:32 +0100

    Full avoider.py program

--- pi2go/avoider.py
+++ pi2go/avoider.py
@@ -1,37 +1,40 @@
-#!/usr/bin/env python
-#Avoids walls based on the sonar and IR sensors
+# Pi2Go 'avoider sketch' - for the second episode of my robot tutorial series
+# This program is fairly simple - it utilises the IR and ultrasonic sensors
+# on the Pi2Go in order to sense obstacles and avoid them
+# Created by Matthew Timmons-Brown and Simon Beal
 
-# Must be run as root - sudo python .py 
-
+# Imports the library
 import pi2go
 
+# Intialises the library
 pi2go.init()
 
-vsn = pi2go.version()
-if vsn == 1:
-  print "Running on Pi2Go (NOT BEEN TRIALLED FOR THIS)"
-else:
-  print "Running on Pi2Go-Lite"
+# Here we set the speed to 40 out of 100 - feel free to change!
+speed = 40
 
-speed = 30
-turn_distance = 3
-turn_amount = 2
-
+# Here is the main body of the program - a lot of while loops and ifs!
+# In order to get your head around it go through the logical steps slowly!
 try:
   while True:
     if pi2go.irLeft():
       while pi2go.irLeft():
+        # While the left sensor is true - spin right
         pi2go.spinRight(speed)
+      # This stops the motors
       pi2go.stop()
     if pi2go.irRight():
       while pi2go.irRight():
+        # While the right sensor is true - spin left
         pi2go.spinLeft(speed)
       pi2go.stop()
     while not (pi2go.irLeft() or pi2go.irRight()):
-      if pi2go.getDistance() <= turn_distance:
-        pi2go.stepSpinR(speed, turn_amount)
-      pi2go.forward(speed)
+      # While the IR sensors aren't detecting anything, try the ultrasonic!
+      if pi2go.getDistance() <= 3:
+        pi2go.spinRight(speed)
+      else:
+        pi2go.forward(speed)
     pi2go.stop()
 
+# Cleans up your program nicely
 finally:
   pi2go.cleanup()
