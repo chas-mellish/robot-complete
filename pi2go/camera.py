5ec8753b49c06081cf768b04f501ce82c9ce30c9
The Raspberry Pi Guy <theraspberrypiguy@gmail.com>
2014-10-14 18:55:06 +0100

    Trying out stuff with the camera

--- pi2go/camera.py
+++ pi2go/camera.py
@@ -1,4 +1,9 @@
+# This is a work in process currently - I am trying to craft an elegant solution to the camera problem!
+# As of current the user is going to need the camera software installed.
+
 import subprocess
+
+subprocess.call("./camera/camera_install.sh", "start")
 
 try:
     subprocess.call("./camera/camera_install.sh", "start")
